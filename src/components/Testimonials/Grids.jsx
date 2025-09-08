bb7b14d5e4372fa5fc42b0232d2793de8793d13a
codecraftwt <codecraftwt@gmail.com>
2025-09-08 14:17:45 +0530

    service management section

--- src/components/Testimonials/Grids.jsx
+++ src/components/Testimonials/Grids.jsx
@@ -20,7 +20,7 @@ export default function Scroll3DSection() {
   const beamRef = useRef(null);
   const activeIsARef = useRef(true);
   const vignetteRef = useRef(null);
-  const videoSources = ["https://www.pexels.com/download/video/4477603/", "https://cdn.pixabay.com/video/2019/04/23/23008-332257750_tiny.mp4", "https://cdn.pixabay.com/video/2025/03/13/264433_tiny.mp4", "https://www.pexels.com/download/video/33388036/"];
+  const videoSources = [Warehouse, Ecommerce, Service, Industry];
 
   useEffect(() => {
     let ctx;
